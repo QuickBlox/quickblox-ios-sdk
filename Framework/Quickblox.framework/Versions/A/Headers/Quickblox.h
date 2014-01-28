88b3a00a80daffdefcf59d80f9050b6a5d21daae
Igor Khomenko <igor@quickblox.com>
2014-01-28 12:42:01 +0200

    updated QB SDK to 1.8.2

--- Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
+++ Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2012 QuickBlox. All rights reserved.
 //
 
-// frameworkVersion 1.8.1
+// frameworkVersion 1.8.2
 
 #import "Core/BaseServiceFramework.h"
 #import "AuthService/AuthServiceFramework.h"
