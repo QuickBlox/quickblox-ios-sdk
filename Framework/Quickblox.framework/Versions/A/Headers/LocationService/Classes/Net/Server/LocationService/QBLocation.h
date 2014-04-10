ed6b4442a5da4fd40e9114d829681626b70f5d1b
Igor Khomenko <igor@quickblox.com>
2014-04-10 16:27:27 +0300

    updated QB SDK to 1.8.4

--- Framework/Quickblox.framework/Versions/A/Headers/LocationService/Classes/Net/Server/LocationService/QBLocation.h
+++ Framework/Quickblox.framework/Versions/A/Headers/LocationService/Classes/Net/Server/LocationService/QBLocation.h
@@ -32,6 +32,21 @@
 + (NSObject<Cancelable> *)createGeoData:(QBLGeoData *)geodata delegate:(NSObject<QBActionStatusDelegate> *)delegate context:(void *)context;
 
 
+/**
+ Create geo data and send push to all users within radius
+ 
+ Type of Result - QBLGeoDataResult
+ 
+ @param geodata An instance of QBLGeoData
+ @param pushText Push Notification text
+ @param radius The distance in meters around your coordinates
+ @param delegate An object for callback, must adopt QBActionStatusDelegate protocol. The delegate is retained.  Upon finish of the request, result will be an instance of QBLGeoDataResult class.
+ @return An instance, which conforms Cancelable protocol. Use this instance to cancel the operation.
+ */
++ (NSObject<Cancelable> *)createGeoData:(QBLGeoData *)geodata andSendPushWithText:(NSString *)pushText toAllUsersWithinRadius:(CGFloat)radius delegate:(NSObject<QBActionStatusDelegate> *)delegate;
++ (NSObject<Cancelable> *)createGeoData:(QBLGeoData *)geodata andSendPushWithText:(NSString *)pushText toAllUsersWithinRadius:(CGFloat)radius delegate:(NSObject<QBActionStatusDelegate> *)delegate context:(void *)context;
+
+
 #pragma mark -
 #pragma mark Get GeoData with ID
 
