cb95428a37e6e9ccc322489a893122579dfbde95
Igor Khomenko <igor@quickblox.com>
2014-07-17 17:36:14 +0300

    added VideoChat sample with custom session

---
+++ sample-videochat-customsession/VideoChat-sample-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleSample-videochat-ios
+//
+//  Created by QuickBlox team on 1/02/13.
+//  Copyright (c) 2013 QuickBlox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
