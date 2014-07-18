b14e81a4b6bfeb277818f323b5f05a2a526b9a89
Igor Khomenko <igor@quickblox.com>
2014-07-18 15:00:36 +0300

    updated QuickBlox SDK to 1.9.1

--- Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
+++ Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
@@ -4,7 +4,7 @@
 //
 //  Copyright (c) 2012 QuickBlox. All rights reserved.
 //
-// frameworkVersion 1.9
+// frameworkVersion 1.9.1
 
 #import "Core/BaseServiceFramework.h"
 #import "AuthService/AuthServiceFramework.h"
