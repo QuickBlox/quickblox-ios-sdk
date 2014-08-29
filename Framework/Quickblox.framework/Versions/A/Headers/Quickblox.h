666a96dbd8a9cd6bccdfdc9077cd551fae99f28d
Igor Khomenko <igor@quickblox.com>
2014-08-29 13:50:08 +0300

    updated SDK to 2.0.1

--- Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
+++ Framework/Quickblox.framework/Versions/A/Headers/Quickblox.h
@@ -4,7 +4,7 @@
 //
 //  Copyright (c) 2012 QuickBlox. All rights reserved.
 //
-// frameworkVersion 2.0
+// frameworkVersion 2.0.1
 
 #import <Quickblox/BaseServiceFramework.h>
 #import <Quickblox/AuthServiceFramework.h>
