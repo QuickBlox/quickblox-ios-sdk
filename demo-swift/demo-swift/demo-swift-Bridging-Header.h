c2c5529ceb96172be5fff66e99da82d6b8d57aea
Igor Khomenko <igor@quickblox.com>
2014-09-23 23:02:52 +0300

    added demo swift app

---
+++ demo-swift/demo-swift/demo-swift-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+@import UIKit;
+@import Foundation;
+@import SystemConfiguration;
+@import MobileCoreServices;
+
+#import <Quickblox/Quickblox.h>
+
+
