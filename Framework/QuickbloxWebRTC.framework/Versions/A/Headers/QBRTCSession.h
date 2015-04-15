0ed24efff6517e0da193f9ce2973bf0f1d1d93c3
Andrey <this.andrey@gmail.com>
2015-04-15 18:44:48 +0300

    quickbloxWebRTC v 1.0.3

---
+++ Framework/QuickbloxWebRTC.framework/Versions/A/Headers/QBRTCSession.h
@@ -0,0 +1,112 @@
+//
+//  QBRTCSession.h
+//  QuickbloxWebRTC
+//
+//  Created by Andrey Ivanov on 05.01.15.
+//  Copyright (c) 2015 QuickBlox Team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "QBRTCTypes.h"
+
+@class QBPeerChannel;
+@class QBRTCVideoTrack;
+
+@interface QBRTCSession : NSObject
+
+/**
+ *  Enable/Disable audio stream
+ */
+@property (assign, nonatomic) BOOL audioEnabled;
+
+/**
+ * Enalbe/Disable video stream
+ */
+@property (assign, nonatomic) BOOL videoEnabled;
+
+/**
+ *  Indicating the physical position of an AVCaptureDevice's hardware on the system.
+ */
+@property (assign, nonatomic, readonly) AVCaptureDevicePosition currentCaptureDevicePosition;
+
+/**
+ * Set audio session category options
+ */
+@property (assign, nonatomic) AVAudioSessionCategoryOptions audioCategoryOptions __attribute__((deprecated("use 'QBSoundRouter'.")));
+
+/**
+ *  Unique session identifier
+ */
+@property (strong, nonatomic, readonly) NSString *ID;
+
+/**
+ *  Caller ID
+ */
+@property (strong, nonatomic, readonly) NSNumber *callerID;
+
+/**
+ *  IDs of opponents in current session
+ */
+@property (strong, nonatomic, readonly) NSArray *opponents;
+
+/**
+ *  Conference type QBConferenceTypeAudio - audio conference, QBConferenceTypeVideo - video conference
+ */
+@property (assign, nonatomic, readonly) QBConferenceType conferenceType;
+
+- (id)init __attribute__((unavailable("init is not a supported initializer for this class.")));
+
+/**
+ *  Start call. Opponent will receive new session signal in QBRTCClientDelegate method 'didReceiveNewSession:userInfo:
+ *
+ * @param userInfo The user information dictionary for the stat call. May be nil.
+ */
+- (void)startCall:(NSDictionary *)userInfo;
+
+/**
+ * Accept call. Opponent's will receive accept signal in QBRTCClientDelegate method 'session:acceptByUser:userInfo:'
+ *
+ * @param userInfo The user information dictionary for the accept call. May be nil.
+ */
+- (void)acceptCall:(NSDictionary *)userInfo;
+
+/**
+ * Reject call. Opponent's will receive reject signal in QBRTCClientDelegate method 'session:rejectedByUser:userInfo:'
+ *
+ * @param userInfo The user information dictionary for the reject call. May be nil.
+ */
+- (void)rejectCall:(NSDictionary *)userInfo;
+
+/**
+ * Hang up. Opponent's will receive hung up signal in QBRTCClientDelegate method 'session:hungUpByUser:userInfo:'
+ *
+ * @param userInfo The user information dictionary for the hang up. May be nil.
+ */
+- (void)hangUp:(NSDictionary *)userInfo;
+
+/**
+ *  Switch Front / Back video input. (Default: Front camera)
+ *
+ *  @param block isFrontCamera YES/NO
+ */
+- (void)switchCamera:(void (^)(BOOL isFrontCamera))block;
+
+/**
+ *  Remote track with opponent ID
+ *
+ *  @param userID ID of opponent
+ *
+ *  @return QBRTCVideoTrack instance
+ */
+- (QBRTCVideoTrack *)remoteVideoTrackWithUserID:(NSNumber *)userID;
+
+/**
+ *  Connection state for opponent
+ *
+ *  @param userID ID of opponent
+ *
+ *  @return ID of opponent
+ */
+- (QBRTCConnectionState)connectionStateForUser:(NSNumber *)userID;
+
+@end
--- QBRTCChatSample/QBRTCChatSample/AppDelegate.m
+++ QBRTCChatSample/QBRTCChatSample/AppDelegate.m
@@ -12,8 +12,8 @@
 #define QB_DEFAULT_ICE_SERVERS 0
 
 const CGFloat kQBRingThickness = 1.f;
-const NSTimeInterval kQBAnswerTimeInterval = 120.f;
-const NSTimeInterval kQBRTCDisconnectTimeInterval = 120.f;
+const NSTimeInterval kQBAnswerTimeInterval = 60.f;
+const NSTimeInterval kQBRTCDisconnectTimeInterval = 30.f;
 
 const NSUInteger kQBApplicationID = 92;
 NSString *const kQBRegisterServiceKey = @"wJHdOcQSxXQGWx5";
@@ -34,7 +34,7 @@ NSString *const kQBAccountKey = @"7yvNe17TnjNUqDoPwfqp";
     //QuickbloxWebRTC preferences
     [QBRTCConfig setAnswerTimeInterval:kQBAnswerTimeInterval];
     [QBRTCConfig setDisconnectTimeInterval:kQBRTCDisconnectTimeInterval];
-    [QBRTCConfig setDialingTimeInterval:20];
+    [QBRTCConfig setDialingTimeInterval:5];
     
     //SVProgressHUD preferences
     [SVProgressHUD setForegroundColor:[UIColor whiteColor]];
