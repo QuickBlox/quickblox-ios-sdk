8275d8710fb7967030bbeb52fce64b286fa536a2
Andrey Moskvin <berkold90@gmail.com>
2015-05-19 17:12:22 +0300

    QMServices now used for authenticating and logging in to chat

---
+++ sample-chat/sample-chat/Services/QBServiceManager.h
@@ -0,0 +1,20 @@
+//
+//  QBServiceManager.h
+//  sample-chat
+//
+//  Created by Andrey Moskvin on 5/19/15.
+//  Copyright (c) 2015 Igor Khomenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <QMAuthService.h>
+#import <QMChatService.h>
+
+@interface QBServiceManager : NSObject
+
++ (instancetype)instance;
+
+@property (nonatomic, readonly) QMAuthService* authService;
+@property (nonatomic, readonly) QMChatService* chatService;
+
+@end
