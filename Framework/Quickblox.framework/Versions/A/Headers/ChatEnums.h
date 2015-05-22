4cb6a5ea97c9a62a279ab96a8d240851e366d629
Andrey Moskvin <berkold90@gmail.com>
2015-05-22 12:55:32 +0300

    Corrected enums, removed QBChatHistoryMessage class directive

--- Framework/Quickblox.framework/Versions/A/Headers/ChatEnums.h
+++ Framework/Quickblox.framework/Versions/A/Headers/ChatEnums.h
@@ -6,33 +6,22 @@
 //  Copyright (c) 2013 QuickBlox. All rights reserved.
 //
 
-enum QBContactListItemStatus{
-    QBContactListItemStatusOnline = 1,
-    QBContactListItemStatusOffline = 0,
-};
-
-enum QBPresenseShow{
+NS_ENUM(NSUInteger, QBPresenseShow) {
     QBPresenseShowAway = 1, // The entity or resource is temporarily away.
     QBPresenseShowChat = 2, // The entity or resource is actively interested in chatting.
     QBPresenseShowDND = 3, // The entity or resource is busy (dnd = "Do Not Disturb").
     QBPresenseShowXA = 4, // The entity or resource is away for an extended period (xa = "eXtended Away").
 };
 
-enum QBPresenseSubscriptionState{
+NS_ENUM(NSUInteger, QBPresenseSubscriptionState){
     QBPresenseSubscriptionStateNone = 1, // the user does not have a subscription to the contact's presence information, and the contact does not have a subscription to the user's presence information
     QBPresenseSubscriptionStateTo = 2, // the user has a subscription to the contact's presence information, but the contact does not have a subscription to the user's presence information
     QBPresenseSubscriptionStateFrom = 3, // the contact has a subscription to the user's presence information, but the user does not have a subscription to the contact's presence information
     QBPresenseSubscriptionStateBoth = 4, //  both the user and the contact have subscriptions to each other's presence information
 };
 
-
-enum QBChatDialogType {
+NS_ENUM(NSUInteger, QBChatDialogType) {
     QBChatDialogTypePublicGroup = 1,
     QBChatDialogTypeGroup = 2,
     QBChatDialogTypePrivate = 3,
 };
-
-enum QBChatHistoryMessageType {
-    QBChatHistoryMessageTypeText = 1
-};
-
