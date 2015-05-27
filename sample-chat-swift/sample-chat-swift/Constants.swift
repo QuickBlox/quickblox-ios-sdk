a7e129e71a0d96815fea8b7d51f2b57d0d4a7fe5
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2015-05-27 12:30:15 +0300

    Fixed issue with constant

--- sample-chat-swift/sample-chat-swift/Constants.swift
+++ sample-chat-swift/sample-chat-swift/Constants.swift
@@ -12,7 +12,7 @@ import Foundation
 //0-release, 1-dev, 2-qa
 let QB_CHAT_SAMPLE_MODE:Int = 2
 let kChatPresenceTimeInterval:NSTimeInterval = 45
-let kDialogsPageLimit:Int = 100
+let kDialogsPageLimit:UInt = 100
 
 class Constants{
     class var QB_VERSION_STR: String {
