709d32eb18aecce05c08f7f144b5060579a7b3d4
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2015-05-28 12:18:46 +0300

    Updated Quickblox framework, added Ad hoc provision

--- Framework/Quickblox.framework/Versions/A/Headers/QBResponsePage.h
+++ Framework/Quickblox.framework/Versions/A/Headers/QBResponsePage.h
@@ -10,10 +10,12 @@
 
 @property (nonatomic) NSInteger skip;
 @property (nonatomic) NSInteger limit;
+@property (nonatomic, readonly) NSUInteger totalEntries;
 
 + (QBResponsePage *)responsePageWithLimit:(NSInteger)limit;
 + (QBResponsePage *)responsePageWithLimit:(NSInteger)limit skip:(NSInteger)skip;
-
 + (QBResponsePage *)responsePageForLastRecord;
+
++ (QBResponsePage *)responsePageWithLimit:(NSInteger)limit skip:(NSInteger)skip totalEntries:(NSUInteger)totalEntries;
 
 @end
