5b9c49e7d3a7e8b893068d19fd5fbf84eabe95b2
Igor Khomenko <igor@quickblox.com>
2015-06-11 12:55:35 +0300

    new push sample is uploaded

--- sample-messages/Pods/BuildHeaders/MTBlockAlertView/MTBlockAlertView.h
+++
@@ -1,1 +0,0 @@
-../../MTBlockAlertView/MTBlockAlertView/MTBlockAlertView/MTBlockAlertView.h
---
+++ sample-messages/sample-messages/AppDelegate.m
@@ -0,0 +1,116 @@
+//
+//  AppDelegate.m
+//  sample-messages
+//
+//  Created by Igor Khomenko on 6/11/15.
+//  Copyright (c) 2015 Igor Khomenko. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <Quickblox/Quickblox.h>
+#import <SVProgressHUD.h>
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    // Set QuickBlox credentials (You must create application in admin.quickblox.com)
+    //
+    [QBApplication sharedApplication].applicationId = 92;
+    [QBConnection registerServiceKey:@"wJHdOcQSxXQGWx5"];
+    [QBConnection registerServiceSecret:@"BTFsj7Rtt27DAmT"];
+    [QBSettings setAccountKey:@"7yvNe17TnjNUqDoPwfqp"];
+    
+#ifndef DEBUG
+    [QBSettings useProductionEnvironmentForPushNotifications:YES];
+#endif
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"didReceiveRemoteNotification userInfo=%@", userInfo);
+    
+    // Get push alert
+    NSString *message = userInfo[QBMPushMessageApsKey][QBMPushMessageAlertKey];
+    NSMutableDictionary *pushInfo = [NSMutableDictionary dictionaryWithObjectsAndKeys:message, @"message", nil];
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"kPushDidReceive" object:nil userInfo:pushInfo];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSString *deviceIdentifier = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    
+    //  New way, only for updated backends
+    //
+    QBMSubscription *subscription = [QBMSubscription subscription];
+    subscription.notificationChannel = QBMNotificationChannelAPNS;
+    subscription.deviceUDID = deviceIdentifier;
+    subscription.deviceToken = deviceToken;
+
+    [QBRequest createSubscription:subscription successBlock:^(QBResponse *response, NSArray *objects) {
+        NSLog(@"Successfull response!");
+        [SVProgressHUD dismiss];
+    } errorBlock:^(QBResponse *response) {
+        NSLog(@"Response error:%@", response.error);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:[response.error description]
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        [SVProgressHUD dismiss];
+    }];
+
+//    // Old way
+//    //
+//    [QBRequest registerSubscriptionForDeviceToken:deviceToken uniqueDeviceIdentifier:deviceIdentifier
+//                                     successBlock:^(QBResponse *response, NSArray *subscriptions) {
+//                                         NSLog(@"Successfull response!");
+//                                     } errorBlock:^(QBError *error) {
+//                                         NSLog(@"Response error:%@", error);
+//                                         
+//                                         
+//                                     }];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                    message:[error localizedDescription]
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
+
+@end
