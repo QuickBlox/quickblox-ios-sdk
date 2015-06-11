6708a0fe21de29d052a8aedb34dea7ec143b5b67
Igor Khomenko <igor@quickblox.com>
2015-06-11 22:22:55 +0300

    removed old users sample

--- sample-users_old/Pods/QuickBlox/Quickblox.framework/Versions/A/Headers/QBXMPP.h
+++
@@ -1,9 +0,0 @@
-#import "QBXMPPJID.h"
-#import "QBXMPPStream.h"
-#import "QBXMPPElement.h"
-#import "QBXMPPIQ.h"
-#import "QBXMPPMessage.h"
-#import "QBXMPPPresence.h"
-#import "QBXMPPModule.h"
-
-#import "NSXMLElement+QBXMPP.h"
