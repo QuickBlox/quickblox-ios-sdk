bc8cb1ef53e34570fa5a925951f483ff7708cfa9
Andrey Moskvin <berkold90@gmail.com>
2015-08-28 14:36:26 +0300

    Added field validation on sign up

--- sample-users/sample-users/SignUpTableViewController.m
+++ sample-users/sample-users/SignUpTableViewController.m
@@ -10,7 +10,8 @@
 #import <Quickblox/Quickblox.h>
 #import <SVProgressHUD.h>
 
-@interface SignUpTableViewController ()
+@interface SignUpTableViewController () <UITextFieldDelegate>
+
 @property (nonatomic, weak) IBOutlet UITextField *loginTextField;
 @property (nonatomic, weak) IBOutlet UITextField *passwordTextField;
 @property (nonatomic, weak) IBOutlet UITextField *confirmationTextField;
@@ -19,21 +20,65 @@
 
 @implementation SignUpTableViewController
 
+- (BOOL)isPasswordConfirmed
+{
+    BOOL confirmed;
+    if (self.passwordTextField.text == nil || self.passwordTextField.text.length == 0) {
+        confirmed = NO;
+    } else if (self.confirmationTextField.text == nil || self.confirmationTextField.text.length == 0) {
+        confirmed = NO;
+    } else {
+        confirmed = [self.passwordTextField.text isEqualToString:self.confirmationTextField.text];
+    }
+    
+    self.passwordTextField.backgroundColor = confirmed ? [UIColor whiteColor] : [UIColor redColor];
+    self.confirmationTextField.backgroundColor = confirmed ? [UIColor whiteColor] : [UIColor redColor];
+    
+    return confirmed;
+}
+
+- (BOOL)isLoginTextValid
+{
+    BOOL loginValid = (self.loginTextField.text != nil && self.loginTextField.text.length > 0);
+    self.loginTextField.backgroundColor = loginValid ? [UIColor whiteColor] : [UIColor redColor];
+    return loginValid;
+}
+
 - (IBAction)signUpButtonTouched:(id)sender
 {
-#warning add validation
-    [SVProgressHUD showWithStatus:@"Signing up"];
+    [self.view endEditing:YES];
     
-    QBUUser *user = [QBUUser new];
-    user.login = self.loginTextField.text;
-    user.password = self.passwordTextField.text;
-    
-    __weak typeof(self)weakSelf = self;
-    [QBRequest signUp:user successBlock:^(QBResponse *response, QBUUser *user) {
-        [QBRequest logInWithUserLogin:user.login password:user.password successBlock:^(QBResponse *response, QBUUser *user) {
-            [SVProgressHUD dismiss];
+    BOOL confirmed = [self isPasswordConfirmed];
+    BOOL nonEmptyLogin = [self isLoginTextValid];
+
+    if (confirmed && nonEmptyLogin) {
+        [SVProgressHUD showWithStatus:@"Signing up"];
+
+        QBUUser *user = [QBUUser new];
+        user.login = self.loginTextField.text;
+        user.password = self.passwordTextField.text;
+        
+        NSString* password = user.password;
+
+        __weak typeof(self)weakSelf = self;
+        [QBRequest signUp:user successBlock:^(QBResponse *response, QBUUser *user) {
+            [QBRequest logInWithUserLogin:user.login password:password successBlock:^(QBResponse *response, QBUUser *user) {
+                [SVProgressHUD dismiss];
+                
+                [weakSelf.navigationController popToRootViewControllerAnimated:YES];
+            } errorBlock:^(QBResponse *response) {
+                [SVProgressHUD dismiss];
+                
+                NSLog(@"Errors=%@", [response.error description]);
+                
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                                message:[response.error  description]
+                                                               delegate:nil
+                                                      cancelButtonTitle:@"OK"
+                                                      otherButtonTitles:nil];
+                [alert show];
+            }];
             
-            [weakSelf.navigationController popToRootViewControllerAnimated:YES];
         } errorBlock:^(QBResponse *response) {
             [SVProgressHUD dismiss];
             
@@ -46,19 +91,14 @@
                                                   otherButtonTitles:nil];
             [alert show];
         }];
-        
-    } errorBlock:^(QBResponse *response) {
-        [SVProgressHUD dismiss];
-        
-        NSLog(@"Errors=%@", [response.error description]);
-        
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                        message:[response.error  description]
-                                                       delegate:nil
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
-        [alert show];
-    }];
+    }
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    textField.backgroundColor = [UIColor whiteColor];
 }
 
 @end
