ee041d638b423fbf7e806caa9422ee5d7ee18e65
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2015-09-21 12:26:36 +0300

    QBIOSSDK-399

---
+++ sample-push-notifications/sample-push-notifications/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  sample-messages
+//
+//  Created by Quickblox Team on 6/11/15.
+//  Copyright (c) 2015 QuickBlox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
++ (void)showAlertViewWithErrorMessage:(NSString *)errorMessage;
+
+@end
+
