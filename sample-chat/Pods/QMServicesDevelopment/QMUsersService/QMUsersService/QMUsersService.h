ef7e9af027e4ce088204ca9ce9b565f81d16556f
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2015-11-04 16:22:04 +0200

    [temp] switched QMServices pods to a specific branch

--- sample-chat/Pods/QMServicesDevelopment/QMUsersService/QMUsersService/QMUsersService.h
+++ sample-chat/Pods/QMServicesDevelopment/QMUsersService/QMUsersService/QMUsersService.h
@@ -57,55 +57,95 @@
 #pragma mark - Intelligent fetch
 
 /**
- *  Retrieve user with id.
+ *  Get user by id.
  *
  *  @param userID   id of user to retreive
  *
  *  @return BFTask with QBUUser as a result
  */
-- (BFTask<QBUUser *> *)retrieveUserWithID:(NSUInteger)userID;
+- (BFTask<QBUUser *> *)getUserWithID:(NSUInteger)userID;
 
 /**
- *  Retrieve users with ids.
+ *  Get users by ids.
  *
  *  @param userIDs  array of user ids
  *
  *  @return BFTask with NSArray of QBUUser instances as a result
  */
-- (BFTask<NSArray<QBUUser *> *> *)retrieveUsersWithIDs:(NSArray<NSNumber *> *)usersIDs;
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithIDs:(NSArray<NSNumber *> *)usersIDs;
 
 /**
- *  Retrieve users with emails.
+ *  Get users by ids with extended pagination parameters.
+ *
+ *  @param userIDs  array of user ids
+ *  @param page     QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithIDs:(NSArray<NSNumber *> *)usersIDs page:(QBGeneralResponsePage *)page;
+
+/**
+ *  Get users by emails.
  *
  *  @param emails   array of user emails
  *
  *  @return BFTask with NSArray of QBUUser instances as a result
  */
-- (BFTask<NSArray<QBUUser *> *> *)retrieveUsersWithEmails:(NSArray<NSString *> *)emails;
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithEmails:(NSArray<NSString *> *)emails;
 
 /**
- *  Retrieve users with facebook ids.
+ *  Get users by emails with extended pagination parameters.
+ *
+ *  @param emails   array of user emails
+ *  @param page     QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithEmails:(NSArray<NSString *> *)emails page:(QBGeneralResponsePage *)page;
+
+/**
+ *  Get users by facebook ids.
  *
  *  @param facebookIDs  array of user facebook ids
  *
  *  @return BFTask with NSArray of QBUUser instances as a result
  */
-- (BFTask<NSArray<QBUUser *> *> *)retrieveUsersWithFacebookIDs:(NSArray<NSString *> *)facebookIDs;
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithFacebookIDs:(NSArray<NSString *> *)facebookIDs;
 
 /**
- *  Retrieve users with logins.
+ *  Get users by facebook ids with extended pagination parameters.
+ *
+ *  @param facebookIDs  array of user facebook ids
+ *  @param page         QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithFacebookIDs:(NSArray<NSString *> *)facebookIDs page:(QBGeneralResponsePage *)page;
+
+/**
+ *  Get users by logins.
  *
  *  @param logins   array of user logins
  *
  *  @return BFTask with NSArray of QBUUser instances as a result
  */
-- (BFTask<NSArray<QBUUser *> *> *)retrieveUsersWithLogins:(NSArray<NSString *> *)logins;
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithLogins:(NSArray<NSString *> *)logins;
+
+/**
+ *  Get users by logins with extended pagination parameters.
+ *
+ *  @param logins   array of user logins
+ *  @param page     QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)getUsersWithLogins:(NSArray<NSString *> *)logins page:(QBGeneralResponsePage *)page;
 
 
 #pragma mark - Search
 
 /**
- *  Search for users with full name.
+ *  Search for users by full name.
  *
  *  @param searchText   user full name
  *
@@ -114,13 +154,33 @@
 - (BFTask<NSArray<QBUUser *> *> *)searchUsersWithFullName:(NSString *)searchText;
 
 /**
- *  Search for users with tags.
+ *  Search for users by full name with extended pagination parameters.
+ *
+ *  @param searchText   user full name
+ *  @param page         QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)searchUsersWithFullName:(NSString *)searchText page:(QBGeneralResponsePage *)page;
+
+/**
+ *  Search for users by tags.
  *
  *  @param tags   array of user tags
  *
  *  @return BFTask with NSArray of QBUUser instances as a result
  */
 - (BFTask<NSArray<QBUUser *> *> *)searchUsersWithTags:(NSArray<NSString *> *)tags;
+
+/**
+ *  Search for users by tags with extended pagination parameters.
+ *
+ *  @param tags   array of user tags
+ *  @param page   QBGeneralResponsePage instance with extended pagination parameters
+ *
+ *  @return BFTask with NSArray of QBUUser instances as a result
+ */
+- (BFTask<NSArray<QBUUser *> *> *)searchUsersWithTags:(NSArray<NSString *> *)tags page:(QBGeneralResponsePage *)page;
 
 @end
 
