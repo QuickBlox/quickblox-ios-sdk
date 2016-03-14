344050bed7798349485a75ca87f771f949a49c6d
Olya Lutsyk <olyalutsyk@MacBook-Pro-Olya.local>
2016-03-14 11:39:51 +0200

    Add OutgoingStickerCell

---
+++ sample-chat/sample-chat/Views/OutgoingStickerCell.h
@@ -0,0 +1,13 @@
+//
+//  OutgoingStickerCell.h
+//  sample-chat
+//
+//  Created by Olya Lutsyk on 3/13/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OutgoingStickerCell : UICollectionViewCell
+@property (nonatomic, weak) IBOutlet UIImageView *stickerImage;
+@end
---
+++ sample-chat/sample-chat/Views/OutgoingStickerCell.m
@@ -0,0 +1,17 @@
+//
+//  OutgoingStickerCell.m
+//  sample-chat
+//
+//  Created by Olya Lutsyk on 3/13/16.
+//  Copyright © 2016 Quickblox. All rights reserved.
+//
+
+#import "OutgoingStickerCell.h"
+
+@implementation OutgoingStickerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
