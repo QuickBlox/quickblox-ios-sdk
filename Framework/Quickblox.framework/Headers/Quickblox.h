ce1e780cdcfb5c5ce8561f45768ef109e4a34508
Vitaliy Gorbachov <vitaliy.gorbachov@quickblox.com>
2016-08-03 14:22:04 +0300

    Updated Quickblox framework

--- Framework/Quickblox.framework/Headers/Quickblox.h
+++ Framework/Quickblox.framework/Headers/Quickblox.h
@@ -16,5 +16,5 @@
 #import <Quickblox/QBContentHeader.h>
 #import <Quickblox/QBChatHeader.h>
 
-//! Framework version 2.7.4.1
+//! Framework version 2.7.5
 FOUNDATION_EXPORT NSString * const QuickbloxFrameworkVersion;
