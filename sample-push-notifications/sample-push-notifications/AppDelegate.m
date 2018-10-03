dd4bc179b6725f497434fab9cfe95a9298c0755b
Vladimir Nybozhinsky <vladimir.nybozhinsky@quickblox.com>
2018-10-03 17:17:27 +0300

    QBIOSSDK-1469 Update Push Notifications sample to fit new Push API ###feature - add UserNotificationsFramework since IOS10

--- sample-push-notifications/sample-push-notifications/AppDelegate.m
+++ sample-push-notifications/sample-push-notifications/AppDelegate.m
@@ -10,13 +10,14 @@
 #import <Quickblox/Quickblox.h>
 #import <SVProgressHUD.h>
 #import "ViewController.h"
+#import <UserNotifications/UserNotifications.h>
 
 const NSUInteger kApplicationID = 72448;
 NSString *const kAuthKey        = @"f4HYBYdeqTZ7KNb";
 NSString *const kAuthSecret     = @"ZC7dK39bOjVc-Z8";
 NSString *const kAccountKey     = @"C4_z7nuaANnBYmsG_k98";
 
-@interface AppDelegate ()
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
 
 @end
 
@@ -58,6 +59,21 @@ NSString *const kAccountKey     = @"C4_z7nuaANnBYmsG_k98";
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+#pragma mark - UNUserNotificationCenterDelegate iOS 10+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
+    NSLog(@"APNS MESSAGE = %@!!!!!!!!!!!!!!!!************************************************",notification.request.content.userInfo);
+    completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)(void))completionHandler{
+    NSLog(@"APNS MESSAGE = %@**********************************************************",response.notification.request.content.userInfo);
+//    if ([UIApplication sharedApplication].applicationState != UIApplicationStateInactive){
+//        return;
+//    }
+    completionHandler();
+}
+
+#pragma mark - UNUserNotificationCenterDelegate iOS 9+
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
     NSLog(@"didReceiveRemoteNotification userInfo=%@", userInfo);
