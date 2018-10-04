b3113219d2b51e99340eb8ef9fda7d03cfd09dc3
Vladimir Nybozhinsky <vladimir.nybozhinsky@quickblox.com>
2018-10-04 19:05:35 +0300

    add Auth

---
+++ sample-conference-videochat-swift/sample-conference-videochat-swift/sample-conference-videochat-swift/Components/Auth/Views/LoadingButton/QBLoadingButton.swift
@@ -0,0 +1,21 @@
+//
+//  QBLoadingButton.swift
+//  sample-conference-videochat-swift
+//
+//  Created by Vladimir Nybozhinsky on 04.10.2018.
+//  Copyright © 2018 QuickBlox. All rights reserved.
+//
+
+import UIKit
+
+class QBLoadingButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
