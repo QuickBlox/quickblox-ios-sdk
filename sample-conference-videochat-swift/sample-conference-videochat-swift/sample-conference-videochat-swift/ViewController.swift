1cdbf3d7d191d548073495cf6f7f47d40811af9b
Vladimir Nybozhinsky <vladimir.nybozhinsky@quickblox.com>
2018-10-04 19:01:03 +0300

    init

---
+++ sample-conference-videochat-swift/sample-conference-videochat-swift/sample-conference-videochat-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-conference-videochat-swift
+//
+//  Created by Vladimir Nybozhinsky on 04.10.2018.
+//  Copyright © 2018 QuickBlox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
