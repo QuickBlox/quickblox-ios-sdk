e2d7aa6901f9c3403b5b977590a953156bbcee1b
Vladimir Nybozhinsky <vladimir.nybozhinsky@quickblox.com>
2018-12-07 16:01:29 +0200

    QBIOSSDK_1526_webrtc_swift 1. Remove old project in the sample-videochat-webrtc-swift directory. 2. Create new sample-videochat-webrtc-swift and add pods such in the conference videochat swift sample 3. Reuse login screen from the conference videochat swift sample

--- sample-videochat-webrtc-swift/sample-videochat-webrtc-swift/AppDelegate.swift
+++ sample-videochat-webrtc-swift/sample-videochat-webrtc-swift/AppDelegate.swift
@@ -2,36 +2,87 @@
 //  AppDelegate.swift
 //  sample-videochat-webrtc-swift
 //
-//  Created by QuickBlox team
+//  Created by Vladimir Nybozhinsky on 12/7/18.
 //  Copyright © 2018 QuickBlox. All rights reserved.
 //
 
 import UIKit
 import Quickblox
+import QuickbloxWebRTC
+import Fabric
+import Crashlytics
+import SVProgressHUD
 
-let kQBApplicationID:UInt = 72448
-let kQBAuthKey = "f4HYBYdeqTZ7KNb"
-let kQBAuthSecret = "ZC7dK39bOjVc-Z8"
-let kQBAccountKey = "C4_z7nuaANnBYmsG_k98"
+struct CredentialsConstant {
+    static let applicationID:UInt = 73803
+    static let authKey = "qp4zDcV8mk29Qp9"
+    static let authSecret = "Hm2KgDE6eeMZHu5"
+    static let accountKey = "uK_8uinNyz8-npTNB6tx"
+}
 
+struct TimeIntervalConstant {
+    static let answerTimeInterval: TimeInterval = 60.0
+    static let dialingTimeInterval: TimeInterval = 5.0
+}
+
+struct AppDelegateConstant {
+    static let enableStatsReports: UInt = 1
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // Set QuickBlox credentials (You must create application in admin.quickblox.com).
-        QBSettings.applicationID = kQBApplicationID;
-        QBSettings.authKey = kQBAuthKey
-        QBSettings.authSecret = kQBAuthSecret
-        QBSettings.accountKey = kQBAccountKey
+        //        Fabric.with([Crashlytics.self])
+        QBSettings.applicationID = CredentialsConstant.applicationID;
+        QBSettings.authKey = CredentialsConstant.authKey
+        QBSettings.authSecret = CredentialsConstant.authSecret
+        QBSettings.accountKey = CredentialsConstant.accountKey
         QBSettings.autoReconnectEnabled = true
+        QBSettings.logLevel = QBLogLevel.debug
+        QBSettings.enableXMPPLogging()
+        QBRTCConfig.setAnswerTimeInterval(TimeIntervalConstant.answerTimeInterval)
+        QBRTCConfig.setDialingTimeInterval(TimeIntervalConstant.dialingTimeInterval)
+        QBRTCConfig.setLogLevel(QBRTCLogLevel.verbose)
         
+        if AppDelegateConstant.enableStatsReports == 1 {
+            QBRTCConfig.setStatsReportTimeInterval(1.0)
+        }
+        
+        SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.clear)
+        QBRTCClient.initializeRTC()
+        
+        // loading settings
+        //        Settings.instance.load()
         return true
     }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
 }
 
---
+++ sample-videochat-webrtc-swift/sample-videochat-webrtc-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-videochat-webrtc-swift
+//
+//  Created by Vladimir Nybozhinsky on 12/7/18.
+//  Copyright © 2018 QuickBlox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
