824dd11ba8293b077f5a399abe41b96582abb2d3
IlliaChemolosov <ilya.chemolosov@quickblox.com>
2021-03-30 14:15:39 +0300

    Refactoring. - pods updated

--- sample-users/Pods/Target Support Files/Pods-sample-users/Pods-sample-users-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sample_users : NSObject
-@end
-@implementation PodsDummy_Pods_sample_users
-@end
--- sample-users/Pods/Target Support Files/Pods-sample-usersTests/Pods-sample-usersTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sample_usersTests : NSObject
-@end
-@implementation PodsDummy_Pods_sample_usersTests
-@end
