5ee8a11de3ddb0521b3ae50740c4a077f83ba035
IlliaChemolosov <42934399+IlliaChemolosov@users.noreply.github.com>
2023-01-19 10:07:07 +0100

    Swift Package

---
+++ Sources/quickblox-ios-sdk/quickblox_ios_sdk.swift
@@ -0,0 +1,6 @@
+public struct quickblox_ios_sdk {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/quickblox-ios-sdkTests/quickblox_ios_sdkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import quickblox_ios_sdk
+
+final class quickblox_ios_sdkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(quickblox_ios_sdk().text, "Hello, World!")
+    }
+}
